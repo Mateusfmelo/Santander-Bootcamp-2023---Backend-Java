839380c3a677b91d95426b42ce8feb0be34db4fa
Mateus Melo <mateusmelo51@gmail.com>
2023-08-22 22:32:30 -0300

    Add files via upload

---
+++ Conhecendo a Linguagem de Programação Java/Aprendendo a Sintaxe Java/java-anatomia-classes/src/edu/mateus/segundasemana/MinhaClasse.java
@@ -0,0 +1,17 @@
+package edu.mateus.segundasemana;
+
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+        
+        String primeiroNome = "Mateus";
+        String segundoNome = "Melo";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
